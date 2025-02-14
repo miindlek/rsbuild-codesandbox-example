a83218fde6abed32bdd06a7b206f989778686220
bratan <miindlek@gmail.com>
2025-02-14 11:50:57 +0000

    Only use one closed request count

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import { useCallback, useState } from "react";
 
 const App = () => {
   const [openCount, setOpenCount] = useState(0);
-  const [successCount, setSuccessCount] = useState(0);
-
-  const [errorCount, setErrorCount] = useState(0);
+  const [closedCount, setClosedCount] = useState(0);
 
   const handleClick = useCallback(() => {
     setOpenCount((value) => value + 1);
 
     fetch("/api/test")
-      .then(() => setSuccessCount((value) => value + 1))
-      .catch((error) => setErrorCount((value) => value + 1))
+      .then(() => setClosedCount((value) => value + 1))
+      .catch((error) => setClosedCount((value) => value + 1))
       .then(() => setOpenCount((value) => value - 1));
   }, []);
 
@@ -21,9 +19,8 @@ const App = () => {
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
       <button onClick={handleClick}>Click here to send a new request</button>
-      <p>Open Requests: {openCount}</p>
-      <p>Successful Requests: {successCount}</p>
-      <p>Failed Requests: {errorCount}</p>
+      <p>Open Requests count: {openCount}</p>
+      <p>Closed Requests count: {closedCount}</p>
     </div>
   );
 };
