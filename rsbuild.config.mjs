775fd10101fa776bce4107bfd3f6242b3a6b0179
bratan <miindlek@gmail.com>
2025-02-14 11:46:43 +0000

    Add button to trigger api requests

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,6 +1,19 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  server: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    proxy: [
+      {
+        context: [`/api`],
+        target: `http://somepagewhichdoesnotexits.com:9000`,
+        changeOrigin: true,
+        secure: false,
+      },
+    ],
+  },
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import './App.css';
+import "./App.css";
+import { useCallback, useState } from "react";
 
 const App = () => {
+  const [openCount, setOpenCount] = useState(0);
+  const [successCount, setSuccessCount] = useState(0);
+
+  const [errorCount, setErrorCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    setOpenCount((value) => value + 1);
+
+    fetch("/api/test")
+      .then(() => setSuccessCount((value) => value + 1))
+      .catch((error) => setErrorCount((value) => value + 1))
+      .then(() => setOpenCount((value) => value - 1));
+  }, []);
+
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <button onClick={handleClick}>Click here to send a new request</button>
+      <p>Open Requests: {openCount}</p>
+      <p>Successful Requests: {successCount}</p>
+      <p>Failed Requests: {errorCount}</p>
     </div>
   );
 };
